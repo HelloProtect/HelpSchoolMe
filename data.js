033c7718cb93173e8b6935da5e3c5dec93aec761
HelloProtect <gianni.blazquez@gmail.com>
2025-11-18 13:28:04 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const ressourcesScolaires = [
+    {
+        nom: "Calculatrice Graphique en Ligne",
+        lien: "https://exemple.com/outil1",
+        description: "Pour les maths complexes, les fonctions et les statistiques.",
+        motsCles: "calcul, graphique, fonctions, statistiques, algèbre, trigonométrie, mathématiques, outils, maths"
+    },
+    {
+        nom: "Dictionnaire de Synonymes et Antonymes",
+        lien: "https://exemple.com/outil2",
+        description: "Améliorez votre vocabulaire, idéal pour la rédaction de dissertations.",
+        motsCles: "vocabulaire, synonyme, antonyme, écriture, rédaction, dissertation, français, langue"
+    },
+    {
+        nom: "Référence de Conjugaison Française",
+        lien: "https://exemple.com/outil3",
+        description: "Maîtrisez les verbes, l'orthographe et la grammaire.",
+        motsCles: "verbes, orthographe, grammaire, conjuguer, temps, accord, langue française"
+    },
+    {
+        nom: "Tutoriels de Programmation (Python)",
+        lien: "https://exemple.com/outil4",
+        description: "Pour l'informatique, le code et les projets scientifiques.",
+        motsCles: "code, informatique, développement, python, programmation, tutoriel, scientifque, info"
+    }
+    // Ajoutez vos futures ressources ici, c'est très simple !
+];
